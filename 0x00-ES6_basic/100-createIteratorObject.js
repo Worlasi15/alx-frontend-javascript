43faca3591ea6c152d30f15c6a34c90068fd8805
Worlasi15 <efyawalasi@gmail.com>
2024-02-06 07:06:28 -0800

    a function named createIteratorObject, that will take into argument a report Object created with the previous function createReportObject

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,16 +1,8 @@
-import createIteratorObject from "./100-createIteratorObject.js";
-import createEmployeesObject from './11-createEmployeesObject.js';
-import createReportObject from './12-createReportObject.js';
+export default function createIteratorObject(report) {
+  let result = [];
+  for (const value of Object.values(report.allEmployees)) {
+    result = [...result, ...value];
+  }
 
-const employees = {
-  ...createEmployeesObject('engineering', ['Bob', 'Jane']),
-  ...createEmployeesObject('marketing', ['Sylvie'])
-};
-
-const report = createReportObject(employees);
-
-const reportWithIterator = createIteratorObject(report);
-
-for (const item of reportWithIterator) {
-  console.log(item);
+  return result;
 }
