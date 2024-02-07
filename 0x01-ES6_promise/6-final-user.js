6b5ea93d4a2ff6b78c5674acd6d6111cc8205c8a
Worlasi15 <efyawalasi@gmail.com>
2024-02-07 13:53:34 -0800

    export a function named handleProfileSignup

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,19 +1,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-const handleProfileSignup = async (firstName, lastName, fileName) => {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
-  try {
-    const [userData, photoData] = await Promise.allSettled([userPromise, photoPromise]);
-    return [
-      { function: 'signUpUser', status: userData.status, value: userData.status === 'fulfilled' ? userData.value : userData.reason },
-      { function: 'uploadPhoto', status: photoData.status, value: photoData.status === 'fulfilled' ? photoData.value : photoData.reason },
-    ];
-  } catch (error) {
-    return [{ status: 'rejected', value: error }];
-  }
-};
-
-export default handleProfileSignup;
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  const result = [];
+  return Promise.allSettled(promises).then((results) => {
+    results.map(({ status, value, reason }) => (
+      result.push({
+        status,
+        value: status === 'rejected' ? reason.toString() : value,
+      })
+    ));
+    return result;
+  });
+}
