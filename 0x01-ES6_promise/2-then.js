9991a758c9994d9366e40782e9d43c08519205ac
Worlasi15 <efyawalasi@gmail.com>
2024-02-06 16:19:03 -0800

    append 3 handlers

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,11 +2,11 @@ function handleResponseFromAPI(promise) {
   return promise
     .then((response) => {
       console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
+      return Promise.resolve({ status: 200, body: 'success' });
     })
     .catch((error) => {
       console.log('Got a response from the API');
-      return new Error();
+      return Promise.reject(new Error());
     });
 }
 
