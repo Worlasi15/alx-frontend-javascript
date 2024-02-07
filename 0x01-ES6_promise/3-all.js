f525da42d4f1829ff2cc56ac56b134d7f08412a9
Worlasi15 <efyawalasi@gmail.com>
2024-02-06 16:23:59 -0800

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,11 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils.js';
 
 function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
-      const [photoResult, userResult] = results;
-      console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
+      const [photoResponse, userResponse] = results;
+      const { body, firstName, lastName } = userResponse;
+      console.log(`${photoResponse.body} ${firstName} ${lastName}`);
     })
     .catch(() => {
       console.error('Signup system offline');
