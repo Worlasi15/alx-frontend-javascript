d305fb505d01d7c09eff11cf8c3104607cd5deda
Worlasi15 <efyawalasi@gmail.com>
2024-02-07 13:44:47 -0800

    handle response promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,9 @@
 function handleResponseFromAPI(promise) {
-  return promise
-    .then((response) => {
-      console.log('Got a response from the API');
-      return Promise.resolve({ status: 200, body: 'success' });
-    })
-    .catch((error) => {
-      console.log('Got a response from the API');
-      return Promise.reject(new Error());
-    });
-}
+  const body = { status: 200, body: 'success' };
 
+  return promise
+    .then(() => body)
+    .catch((error) => error)
+    .finally(() => console.log('Got a response from the API'));
+}
 export default handleResponseFromAPI;
