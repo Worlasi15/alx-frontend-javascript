4ee29cf75c16fe378e99c0c04753afe3a6038f90
Worlasi15 <efyawalasi@gmail.com>
2024-02-06 16:38:30 -0800

    try block

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,13 +1,11 @@
-import divideFunction from './8-try';
-
-export default function guardrail(mathFunction) {
+function guardrail(mathFunction) {
   const queue = [];
 
   try {
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    queue.push(error.message);
+    queue.push(`Error: ${error.message}`);
   } finally {
     queue.push('Guardrail was processed');
   }
@@ -15,5 +13,4 @@ export default function guardrail(mathFunction) {
   return queue;
 }
 
-console.log(guardrail(() => divideFunction(10, 2)));
-console.log(guardrail(() => divideFunction(10, 0)));
+export default guardrail;
