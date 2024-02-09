ffc0364e101333c1004c6526ae6db5d4f560bb8b
Worlasi15 <efyawalasi@gmail.com>
2024-02-08 16:26:01 -0800

    class holberton

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,45 +1,18 @@
-export default class HolbertonCourse {
-  constructor(name, length, students) {
-    if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
-      throw new TypeError('Invalid attribute types');
-    }
+import HolbertonCourse from "./2-hbtn_course.js";
 
-    // Use underscore attribute versions for storage
-    this._name = name;
-    this._length = length;
-    this._students = students;
-  }
+const c1 = new HolbertonCourse("ES6", 1, ["Bob", "Jane"]);
+console.log(c1.name);
+c1.name = "Python 101";
+console.log(c1);
 
-  // Getter and setter for 'name'
-  get name() {
-    return this._name;
-  }
-  set name (newName) {
-    if (typeof newName !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = newName;
-  }
+try {
+  c1.name = 12;
+} catch(err) {
+  console.log(err.message);
+}
 
-  // Getter and setter for 'length'
-  get length() {
-    return this._length;
-  }
-  set length (newLength) {
-    if (typeof newLength !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    this._length = newLength;
-  }
-
-  // Getter and setter for 'students'
-  get students() {
-    return this._students;
-  }
-  set students (newStudents) {
-    if (!Array.isArray(newStudents)) {
-      throw new TypeError('Students must be an array of strings');
-    }
-    this._students = newStudents;
-  }
+try {
+  const c2 = new HolbertonCourse("ES6", "1", ["Bob", "Jane"]);
+} catch(err) {
+  console.log(err.message);
 }
