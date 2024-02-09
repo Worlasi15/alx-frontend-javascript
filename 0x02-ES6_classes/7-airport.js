5459f09ffec225fe2c955d506f3cefba4052d222
Worlasi15 <efyawalasi@gmail.com>
2024-02-08 16:02:15 -0800

    class airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,18 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+}
