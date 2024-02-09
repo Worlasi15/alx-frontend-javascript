91e11884cae7d599271bad29978361256cc8c9de
Worlasi15 <efyawalasi@gmail.com>
2024-02-08 16:36:25 -0800

    implementing building class

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -12,12 +12,12 @@ export default class SkyHighBuilding extends Building {
 
   set floors(newFloors) {
     if (typeof newFloors !== 'number') {
-      throw TypeError('Floors must be a number');
+      throw new TypeError('Floors must be a number');
     }
     this._floors = newFloors;
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
