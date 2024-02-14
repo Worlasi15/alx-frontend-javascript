4b5ed0c876275cd334a63fe3978a0abf0b1da1d4
Worlasi15 <efyawalasi@gmail.com>
2024-02-13 16:13:18 -0800

    function named createInt8TypedArray that returns a new ArrayBuffer with an Int8 value at a specific position.

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) {
-    throw new Error('Position outside range');
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
+
+  if (position >= 0 && position < length) {
+    int8Array[position] = value;
+    return new DataView(buffer);
   }
 
-  let buffer = new ArrayBuffer(length);
-  let view = new DataView(buffer);
-  view.setInt8(position, value);
-
-  return view;
+  throw new Error('Position outside range');
 }
