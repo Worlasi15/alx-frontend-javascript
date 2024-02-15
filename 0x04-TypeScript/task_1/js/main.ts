38e5205f950004dd98b10112351cd54ccc25ef34
Worlasi15 <efyawalasi@gmail.com>
2024-02-14 18:25:29 -0800

    interfaces director and teacher, function print teacher and class student

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -4,16 +4,58 @@ interface Teacher {
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [key: string]: any; // Allow any additional attributes
+  [key: string]: any;
 }
 
-// Creating a Teacher object
-const teacher3: Teacher = {
-  firstName: 'John',
-  fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
-  contract: false, // Adding an additional attribute
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  const initial = firstName.charAt(0).toUpperCase();
+  const fullLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+  return `${initial}. ${fullLastName}`;
 };
 
-console.log(teacher3);
+// Example usage
+const printedTeacher = printTeacher("John", "Doe");
+console.log(printedTeacher);
+
+interface StudentClassConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+// Interface definition for StudentClass
+interface StudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Class definition for StudentClass
+class StudentClass implements StudentClass {
+  private firstName: string;
+  private lastName: string;
+
+  constructor({ firstName, lastName }: StudentClassConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Example usage
+const studentInstance = new StudentClass({ firstName: 'John', lastName: 'Doe' });
+console.log(studentInstance.workOnHomework()); // Should print "Currently working"
+console.log(studentInstance.displayName()); // Should print "John"
